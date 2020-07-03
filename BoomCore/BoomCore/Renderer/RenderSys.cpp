55d5e35e85cba2d19b0d1c4d8d0575dfe8ae2fe0
JiaqiJin96 <fdsam@hotmail.com>
2020-07-03 17:06:40 +0200

    render class creation

--- BoomCore/BoomCore/Renderer/RenderSys.cpp
+++ BoomCore/BoomCore/Renderer/RenderSys.cpp
@@ -2,5 +2,31 @@
 
 namespace Kawaii
 {
+	void RenderSys::initialize(int width, int height)
+	{
+		m_width = width;
+		m_height = height;
+		
+		m_meshMgr = MeshMgr::getSingleton();
+		m_shaderMgr = ShaderMgr::getSingleton();
+		m_textureMgr = TextureMgr::getSingleton();
+		m_renderList = std::make_shared<RenderSys>();
+
+	}
+
+	void RenderSys::setSkyDome(const std::string& path, const std::string& pFix)
+	{
+		if (m_skyDome != nullptr)
+			return;
+
+		unsigned int skyboxShader = m_shaderMgr->loadShader("skybox", 
+			"Shaders/skybox.vs", "Shaders/skybox.fs");
+		unsigned int cubeTex = m_textureMgr->loadTextureCube("skybox", path, pFix);
+		unsigned int meshIndex = m_meshMgr->loadMesh(new Sphere(1.0f, 10, 10));
+		//passing all infomation to Skydome
+		m_skyDome = std::make_shared<SkyDome>(skyboxShader);
+		m_skyDome->addMesh(meshIndex);
+		m_skyDome->addTexture(cubeTex);
+	}
 
 }
--- BoomCore/BoomCore/Renderer/RenderSys.h
+++ BoomCore/BoomCore/Renderer/RenderSys.h
@@ -3,6 +3,7 @@
 #include "Light.h"
 #include "Camera/Camera3D.h"
 #include "Manager/ShaderMgr.h"
+#include "Manager/MeshMgr.h"
 #include "Manager/TextureMgr.h"
 #include "RenderTarget/RenderTarget.h"
 #include "Manager/FrameBuffer.h"
@@ -12,12 +13,42 @@ namespace Kawaii
 	class RenderSys
 	{
 	private:
-		DirectionalLight::ptr m_sunLight;
-
 		int m_width, m_height;
 		FrameBuffer::ptr m_FrameBuffer;
 
+		DirectionalLight::ptr m_sunLight;
+		SkyDome::ptr m_skyDome;
+
+		MeshMgr::ptr m_meshMgr;
+		Camera3D::ptr m_camera;
+		Camera3D::ptr m_lightCamera;
+		ShaderMgr::ptr m_shaderMgr;
+		TextureMgr::ptr m_textureMgr;
+		RenderTargetList::ptr m_renderList;
+
 	public:
+		typedef std::shared_ptr<RenderSys> ptr;
+	
+		RenderSys() = default;
+		~RenderSys() = default;
+
+		//Initization
+		void initialize(int width, int height);
+		void setSkyDome(const std::string& path, const std::string& pFix);
+		Camera3D::ptr createCamera(glm::vec3 pos, glm::vec3 target);
+
+		//getter
+		Camera3D::ptr getCamera() { return m_camera; }
+		MeshMgr::ptr getMeshMgr() { return m_meshMgr; }
+		ShaderMgr::ptr getShaderMgr() { return m_shaderMgr; }
+		TextureMgr::ptr getTextureMgr() { return m_textureMgr; }
+
+		//setter
+		void addRenderTarget(RenderTarget* target) { m_renderList->addRender(target); }
+
+		void render();
+
+	private:
 
 	};
 }
